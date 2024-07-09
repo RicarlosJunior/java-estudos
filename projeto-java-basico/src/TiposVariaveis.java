d9bde81ca6546760965eb03f1727ab1efe82ea7d
Ricarlos Junior <ricarlosjr@hotmail.com>
2024-07-09 20:46:52 -0300

    Regras para criação de variavel em Java

--- projeto-java-basico/src/TiposVariaveis.java
+++ projeto-java-basico/src/TiposVariaveis.java
@@ -1,4 +1,19 @@
 public class TiposVariaveis {
+    
+    //Conceito basico de variavel (Variavel é um espaço reservado na memoria)
+    //No Java NÃO é permitido:
+
+    //Criar variavel iniciando com numero ex: int 1pacote = 0;
+    //Criar variavel com espaço quando o nome é composto ex: int primeiro pacote = 0;
+    //Criar variavel com caracter especiais exceto (_ $) ex: primeiro/pacote = 0;
+    //Criar variabel com palavras reservadas pela linguagem ex: int long = 0;
+
+    //Agora vamos corrigir os exemplos acima
+    //int primeiroPacote = 0;
+    //int primeiroPacote = 0;
+    //int primeiro$pacote = 0; ou int primeiro_pacote =0; obs nome so para estudo!
+    //int longo - 0;
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
