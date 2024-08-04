73608f6e4009aef5029455e4ed90754cfcc464cd
Ricarlos Junior <ricarlosjr@hotmail.com>
2024-08-04 13:17:00 -0300

    Atualizando repositorio remoto

--- projeto-api-stream/src/App.java
+++ projeto-api-stream/src/App.java
@@ -1,15 +1,10 @@
-import java.util.Arrays;
-import java.util.List;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
     
-        //Vamos explorar API Stream do java!
 
-        //Laço de repetição
-        List<Integer> listaIdade = Arrays.asList(1, 12, 17, 32, 39); 
-        listaIdade.forEach(idade -> System.out.println(idade));
-
+       
 
     }
 }
