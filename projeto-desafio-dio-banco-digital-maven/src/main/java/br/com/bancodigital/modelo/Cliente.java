5512a77beef0edc8b8bc283da5ebb7c7d2cf3c42
Ricarlos Junior <ricarlosjr@hotmail.com>
2024-08-11 23:25:30 -0300

    Atualização Classe Cliente

--- projeto-desafio-dio-banco-digital-maven/src/main/java/br/com/bancodigital/modelo/Cliente.java
+++ projeto-desafio-dio-banco-digital-maven/src/main/java/br/com/bancodigital/modelo/Cliente.java
@@ -1,23 +1,19 @@
 package br.com.bancodigital.modelo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Setter
 @Getter
+@AllArgsConstructor
 public class Cliente {
 
     private int codigo;
     private String nome;
     private String cpf;
     private String endereco;
-
-    public Cliente(int codigo, String nome, String cpf, String endereco) {
-        this.codigo = codigo;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.endereco = endereco;
-    }
 
     
 }
