bd441bce58910a6f007ae818241e1784c1a1dda6
Ricarlos Junior <ricarlosjr@hotmail.com>
2024-08-21 20:43:01 -0300

    Atualização Repositorio

---
+++ projeto-desafio-dio-conta-bancaria-console/src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import model.ContaTermial;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        ContaTermial contaTermial = new ContaTermial();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, informe seu Nome: ");
+        contaTermial.setNomeCliente(scanner.nextLine());
+
+        System.out.print("Por favor, informe a Agência: ");
+        contaTermial.setAgencia(scanner.nextLine());
+
+        System.out.print("Por favor, informe o Numero da Conta: ");
+        contaTermial.setNumero(scanner.nextInt());
+
+        System.out.print("Por favor, informe o Saldo: ");
+        contaTermial.setSaldo(scanner.nextDouble());
+        
+        scanner.close();
+
+        System.out.print(String.format("Olá %s obrigado por criar uma conta em nosso banco, sua Agência é %s Conta %d e seu saldo %.2f já está disponivel.", 
+                            contaTermial.getNomeCliente(), 
+                            contaTermial.getAgencia(), 
+                            contaTermial.getNumero(), 
+                            contaTermial.getSaldo()));
+    }
+}
---
+++ projeto-desafio-dio-conta-bancaria-console/src/model/ContaTermial.java
@@ -0,0 +1,35 @@
+package model;
+
+public class ContaTermial {
+
+    private String nomeCliente;
+    private String agencia;
+    private int numero;
+    private double saldo;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
